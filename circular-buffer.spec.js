f26190d105139189643e29fee84de4cf9b226955
Eduardo Esmanhoto <eduardo_esmanhoto@hotmail.com>
2022-04-07 18:47:08 -0300

    new reading and writing tests added

--- circular-buffer.spec.js
+++ circular-buffer.spec.js
@@ -1,17 +1,33 @@
 import CircularBuffer, {
   BufferFullError,
   BufferEmptyError,
-} from './circular-buffer';
+} from "./circular-buffer";
 
-describe('CircularBuffer', () => {
-  test('reading empty buffer should fail', () => {
+describe("CircularBuffer", () => {
+  test("reading empty buffer should fail", () => {
     const buffer = new CircularBuffer(1);
     expect(() => buffer.read()).toThrow(BufferEmptyError);
   });
 
-  test('can read an item just written', () => {
-    const buffer = new CircularBuffer(1);
-    buffer.write('1');
-    expect(buffer.read()).toBe('1');
+  test("reading (bigger) empty buffer should fail", () => {
+    const buffer = new CircularBuffer(3);
+    expect(() => buffer.read()).toThrow(BufferEmptyError);
   });
+
+  test("can read an item just written", () => {
+    const buffer = new CircularBuffer(1);
+    buffer.write("1");
+    expect(buffer.read()).toBe("1");
+  });
+});
+
+test("reading 3 recently written items", () => {
+  const buffer = new CircularBuffer(3);
+  buffer.write("1");
+  // console.log(buffer);
+  buffer.write("2");
+  // console.log(buffer);
+  buffer.write("3");
+  // console.log(buffer);
+  expect(buffer.read()).toBe("3");
 });
